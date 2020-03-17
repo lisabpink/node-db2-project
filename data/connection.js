3418e84b9b521921ac87d6b3f4beac3a8ac54fe8
LISA_COOK <lisabpink@gmail.com>
2020-03-17 17:24:34 -0400

    seeds completed

--- data/connection.js
+++
@@ -1,8 +0,0 @@
-// const knex = require('knex');
-
-// const knexfile = require('../knexfile.js');
-
-// const knexConfig = knexfile.development;
-
-// //db represents a live connection to the database
-// module.exports =   knex(knexConfig);
